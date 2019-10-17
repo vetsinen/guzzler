99dc5695c000d7fcff33b414970855759e56bdfa
vetsinen <vetsinen@gmail.com>
2019-10-17 17:57:54 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+echo 'Hello';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'http://httpbin.org',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
